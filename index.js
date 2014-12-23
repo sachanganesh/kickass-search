c2ded5f7bd708083ed92a10ae58e81e2756dcac4
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-12-22 16:37:46 -0800

    Fix jscs code styling issues

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var Q = require('q');
-var request = require('request');
+var Q = require('q'),
+    request = require('request');
 
 exports.search = function (category, query) {
-    var deferred = Q.defer();
-    var url = 'https://www.kimonolabs.com/api/';
+    var deferred = Q.defer(),
+        url = 'https://www.kimonolabs.com/api/';
     if (category && query)
-        url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5'
-                + '&kimpath2=' + query + ' category:' + category;
+        url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath2=' + query + ' category:' + category;
     else if (!category.length && query)
-        url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5'
-                + '&kimpath2=' + query;
+        url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath2=' + query;
     else if (category && !query)
-        url += '7y1mi6wa?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5'
-                + '&kimpath1=' + category;
+        url += '7y1mi6wa?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath1=' + category;
     else if (!category && !query)
-        url += '7y1mi6wa?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5'
-                + '&kimpath1=new';
+        url += '7y1mi6wa?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath1=new';
     request(url, function (err, resp, body) {
         if (err) {
             console.log(err);
             deferred.reject(JSON.parse(err));
         } else {
-            var data = JSON.parse(body);
-            var files = data.results.files;
+            var data = JSON.parse(body),
+                files = data.results.files;
             files.forEach(function (file) {
                 file.magnet.href = file.magnet.href.substring(file.magnet.href.indexOf('magnet:?xt'));
             });
