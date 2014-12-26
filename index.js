b2eb476d23b0cc2e6908c348bec2c68f5c10c465
Sachan Ganesh <sachan.ganesh@gmail.com>
2014-12-25 19:29:08 -0800

    Remove loose .length call

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.search = function (category, query) {
     , url = 'https://www.kimonolabs.com/api/';
     if (category && query) {
         url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath2=' + query + ' category:' + category;
-    } else if (!category.length && query) {
+    } else if (!category && query) {
         url += '2egu2p5g?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath2=' + query;
     } else if (category && !query) {
         url += '7y1mi6wa?apikey=JTymD4IrXDKPLFth0vI8GhoR8GEwGXo5' + '&kimpath1=' + category;
